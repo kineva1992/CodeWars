80245ad8f25404e015b7ba37b01217d0fd929677
alexandex <kienva1992@gmail.com>
2021-04-13 16:12:28 +0300

    feat: add new project Swapper

---
+++ kyu8/Swapper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Swapper
+{
+    class Program
+    {
+        public object[] Arguments { get; private set; }
+
+        public Program(object[] args)
+        {
+          Arguments = args;
+        }
+
+        public void SwapValues() => Array.Reverse(Arguments);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
