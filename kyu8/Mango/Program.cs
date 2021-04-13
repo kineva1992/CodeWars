ac83ae785627bda5a1e85fcde4d90e2937a405dc
alexandex <kienva1992@gmail.com>
2021-04-13 15:10:48 +0300

    feat: Add Mango

---
+++ kyu8/Mango/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mango
+{
+    class Program
+    {
+
+        public static int Mango(int quantity, int price) {
+
+            var qty = quantity - Math.Floor((double)quantity / 3);
+
+            return (int)qty * price;
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
