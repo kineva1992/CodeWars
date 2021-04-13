2d3986e5456d0505cd956a24aa08aa9373a4453e
alexandex <kienva1992@gmail.com>
2021-04-13 14:55:36 +0300

    feat: add project PreFizz

---
+++ kyu8/PreFizz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace PreFizz
+{
+    class Program
+    {
+        public static int[] PreFizz(int n) {
+
+            int start = 1;
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = start;
+                start++;
+            }
+
+            return result;
+        }
+
+        public static int[] PreFizzV2(int n) {
+
+            return Enumerable.Range(1, n).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(PreFizzV2(5));
+            Console.ReadKey();
+        }
+    }
+}
