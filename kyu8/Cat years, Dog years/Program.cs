dbf318d4e584dd9571bfcb38dd13fbe7637f272d
alexandex <kienva1992@gmail.com>
2021-04-15 19:20:34 +0300

    feat: add Cat_years__Dog_years, and  Replace all dot

---
+++ kyu8/Cat years, Dog years/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Cat_years__Dog_years
+{
+    class Program
+    {
+        /*
+         I have a cat and a dog.
+
+    I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+    Return their respective ages now as [humanYears,catYears,dogYears]
+
+    NOTES:
+
+    humanYears >= 1
+    humanYears are whole numbers only
+    Cat Years
+    15 cat years for first year
+    +9 cat years for second year
+    +4 cat years for each year after that
+    Dog Years
+    15 dog years for first year
+    +9 dog years for second year
+    +5 dog years for each year after that
+
+         */
+
+        public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+
+            var catYers = 0;
+            var dogYers = 0;
+
+            for (int i = 1; i > humanYears + 1; i++) {
+                switch (i) {
+                    case 1:
+                        catYers += 15;
+                        dogYers += 15;
+                        break;
+                    case 2:
+                        catYers += 9;
+                        dogYers += 9;
+                        break;
+                    default:
+                        catYers += 4;
+                        dogYers += 5;
+                        break;
+                            
+                }
+            }
+
+
+            return new int[] {humanYears, catYers, dogYers };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(humanYearsCatYearsDogYears(2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kyu8/Replace all dots/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Replace_all_dots
+{
+    class Program
+    {
+        /*
+        The code provided is supposed replace all the dots . in the specified String str with dashes -
+        But it's not working properly.
+
+        Task
+        Fix the bug so we can all go home early.
+
+        Notes
+        String str will never be null.
+         */
+        public static string ReplaceDots(string str)
+        {
+            return new Regex("\\.").Replace(str, "-");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ReplaceDots("one.two.three"));
+            Console.ReadKey();
+        }
+    }
+}
