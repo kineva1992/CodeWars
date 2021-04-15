be72adfd3fc75cd81c7dee0ee7128f8930e7ce03
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-04-15 13:15:32 +0300

    Update Function.cs

--- kyu8/PowersOfTwo/Function.cs
+++ kyu8/PowersOfTwo/Function.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace PowersOfTwo
 {
+    /*
+    Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+    */
+    
     class Function
     {
         public static BigInteger[] PowersOfTwo(int n)
