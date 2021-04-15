735930830a543db1eca090a5abc84432ca5fde36
alexandex <kienva1992@gmail.com>
2021-04-15 19:30:34 +0300

    feat: add

---
+++ kyu8/Convert number to reversed array of digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Convert_number_to_reversed_array_of_digits
+{
+    /*
+     Convert number to reversed array of digits
+     Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+     */
+
+    public static long[] Digitize(long n)
+    {
+        int[] nnumber = new int[n];
+
+        var longs = nnumber.Select(item => (long)item).ToArray();
+        return longs;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
