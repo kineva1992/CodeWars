4f36c44737c1d5ba0a3d46365c94b82ff7f62aa4
alexandex <kienva1992@gmail.com>
2021-04-17 12:43:59 +0300

    feat; add new folder 7 kuy

---
+++ kyu7/JavaScript Array Filter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JavaScript_Array_Filter
+{
+    class Program
+    {
+        /*Starting with .NET Framework 3.5, C# supports a Where (in the System.Linq namespace) 
+         * method which allows a user to filter arrays based on a predicate. 
+         * Use the Where method to complete the function given.*/
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            List<int> newNumber = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if ((numbers[i] % 1) == 0) {
+
+                    newNumber.Add(numbers[i]);
+                }
+
+            }
+            return newNumber.ToArray();
+        }
+
+        public static int[] GetEvenNumbersVer2(int[] numbers) => numbers.Where(n => n % 2 == 0).ToArray();
+        
+
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,2,3,11,1,1,2};
+
+
+
+            Console.WriteLine(GetEvenNumbersVer2(numbers));
+        }
+    }
+}
