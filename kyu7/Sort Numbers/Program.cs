6cb815f7df60831b6533822e8326024f0a3d3093
alexandex <kienva1992@gmail.com>
2021-04-18 17:15:57 +0300

    feat: add new project

---
+++ kyu7/Sort Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Numbers
+{
+    /*Finish the solution so that it sorts the passed in array of numbers. 
+     * If the function passes in an empty array or null/nil value then it should return an empty array.*/
+
+    class Program
+    {
+        public static int[] SortNumbers(int[] nums)
+        {
+            return nums?.OrderBy(x => x).ToArray() ?? new int[0];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
