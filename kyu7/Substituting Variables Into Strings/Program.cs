9db662a9bbca2d02c3c99b5b4568f5a904d1df93
alexandex <kienva1992@gmail.com>
2021-04-18 17:34:33 +0300

    feat: add new project Substituting_Variables_Into_Strings

---
+++ kyu7/Substituting Variables Into Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Substituting_Variables_Into_Strings
+{
+    class Program
+    {
+        public static string Solution(int value)
+        {
+            string result = value.ToString().PadLeft(5, '0');
+            return $"Value is {result}";
+        }
+
+        public static string SolutionVer2(int value)
+        {
+            return $"Value is {value:D5}";
+        }
+
+        public static string SolutionVer3(int value)
+        {
+            var result = value.ToString();
+
+            while (result.Length > 5) {
+                result = "0" + result;
+            }
+            return $"Value is {result}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(55555));
+        }
+    }
+}
