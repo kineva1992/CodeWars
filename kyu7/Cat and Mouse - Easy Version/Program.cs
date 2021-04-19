cd70c10ad724b78872fe16dec0a0e06b92465847
alexandex <kienva1992@gmail.com>
2021-04-19 20:59:14 +0300

    feat: add new project Cat_and_Mouse___Easy_Version

---
+++ kyu7/Cat and Mouse - Easy Version/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Cat_and_Mouse___Easy_Version
+{
+    class Program
+    {
+        public static string CatMouse(string x) {
+            int start;
+            start = x.Length;
+
+            return start - 2 > 3 ? "Escaped!" : "Caught!";
+        }
+
+        public static string CatMouseVer2(string x) => x.Count(c => c == '.') > 3 ? "Escaped!" : "Caught!";
+
+        public static string CatMouseVer3(string x) {
+            int count;
+
+            count = 0;
+            foreach (char item in x) {
+                if (item == '.') {
+                    count++;
+                }
+            }
+
+            return count < 3 ? "Caught!" : "Escaped!";
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
