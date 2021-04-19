86e5eb87bb0850f21797929f54e9c2bf60d693c5
alexandex <kienva1992@gmail.com>
2021-04-19 21:28:54 +0300

    feat: add new project Credit_Card_Mask

---
+++ kyu7/Credit Card Mask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Credit_Card_Mask
+{
+    class Program
+    {
+        public static string Maskify(string cc) {
+            
+            string addString;
+            string fourEndNumber;
+
+            if (cc.Length < 4) return cc;
+
+            
+            fourEndNumber = cc.Substring(cc.Length - 4);
+            addString = "";
+            for (int i = 0; i < cc.Length - 4; i++) {
+                addString += "#";
+            }
+            return addString + fourEndNumber;
+        }
+
+        public static string MaskifyVer2(string cc)
+        {
+            int len = cc.Length;
+            if (len <= 4) {
+                return cc;
+            }
+
+            return cc.Substring(len - 4).PadLeft(len, "#")
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            string str = "4556364607935616";
+
+
+            Console.WriteLine(Maskify(str));
+        }
+    }
+}
