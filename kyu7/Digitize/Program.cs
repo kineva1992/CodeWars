9695a8fcee03d0ca09af6ffce43c0ebadbe4d14b
alexandex <kienva1992@gmail.com>
2021-04-20 19:43:47 +0300

    feat:add new project Digitize

---
+++ kyu7/Digitize/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Digitize
+{
+    class Program
+    {
+        public static int[] digitize(int n) {
+            if (n == 0) {
+                return new int[1] { 0 };
+            }
+
+            List<int> digit = new List<int>();
+
+
+            for (; n != 0; n /= 10) {
+                digit.Add(n % 10);
+            }
+
+            int[] array = digit.ToArray();
+
+            Array.Reverse(array);
+            return array;
+        }
+
+        public static int[] digitizeVer2(int n)
+        {
+            return n.ToString().Select(ch => Int32.Parse(ch.ToString())).ToArray();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
