54d31ab410be189e9891288a89a9df8526f2b12c
alexandex <kienva1992@gmail.com>
2021-04-21 20:56:37 +0300

    Add new project

---
+++ kyu7/Driving Licence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Driving_Licence
+{
+    class Program
+    {
+        public string driver(params string[] data)
+        {
+            string date = DateTime.Parse(data[3]).ToString("ddMMyyyy");
+            return data[2].PadRight(5, '9').Substring(0, 5).ToUpper() + data[6] + ((data[4] == "F") ? (date[2] - 43).ToString() + date[3] : date.Substring(2, 2)) + date.Substring(0, 2)
+                + date[7] + data[0][0] + ((data[1].Length != 0) ? data[0][1] : '9') + "9AA";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ kyu8/Sort and Star/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_and_Star
+{
+    /*
+     You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+    The returned value must be a string, and have "***" between each of its letters.
+
+    You should not remove or add elements from/to the array.
+     */
+
+    
+    }
+
+    class Program
+    {
+    public static string TwoSort(string[] s) {
+        Array.Sort(s, StringComparer.Ordinal);
+        return string.Join("***", s.First().ToArray());
+    }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+
