285c2f510f0c5b3a01b3f0d88a172d6bae0640fe
alexandex <kienva1992@gmail.com>
2021-04-23 21:49:39 +0300

    1

---
+++ kyu7/All Star Code Challenge 22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace All_Star_Code_Challenge_22
+{
+    class Program
+    {
+       
+
+    public static class Kata
+    {
+        public static string ToTime(int seconds)
+        {
+                var hour = Math.Floor((decimal)seconds/3600);
+                var minutes = Math.Floor((seconds - (hour * 3600)) / 60);
+
+
+            return $"{hour} hour(s) and {minutes} minute(s)";
+        }
+    }
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
