a64952bf711ddeb68bd27615292c6cf2350f9bc2
alexandex <kienva1992@gmail.com>
2021-07-25 17:30:23 +0300

    1

---
+++ kyu7/LeapYears/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeapYears
+{
+    class Program
+    {
+        public static bool isLeapYesr(int yers) => DateTime.IsLeapYear(yers);
+
+        public static bool IsLeapYearVer2(int year)
+        {
+            if (year % 400 == 0) return true;1
+            if (year % 100 == 0) return false;
+            return year % 4 == 0;
+        }
+        public static bool IsLeapYearVer3(int year)
+        {
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
