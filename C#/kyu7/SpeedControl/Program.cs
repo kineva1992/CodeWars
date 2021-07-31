b9bca8759a555491502d2099f43cec54dcb69f67
alexandex <kienva1992@gmail.com>
2021-07-31 17:43:35 +0300

    1

---
+++ C#/kyu7/SpeedControl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SpeedControl
+{
+    public class GpsSpeed
+    {
+        public static int Gps(int s, double[] x)
+        {
+            if (x.Length < 2) return 0;
+            double[] dx = new double[x.Length];
+            dx[0] = (x[0] - 0) / s * 3600;
+            for (int i = 1; i < x.Length; i++)
+            {
+                dx[i] = (x[i] - x[i - 1]) / s * 3600;
+            }
+
+            return (int)Math.Floor(dx.Max());
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
