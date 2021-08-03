14f9332e0e2ba8cd6cec0ecf3b886e782c7e1d2e
alexandex <kienva1992@gmail.com>
2021-08-03 18:21:40 +0300

    123

--- JavaScript/dist/move10.dev.js
+++ JavaScript/dist/move10.dev.js
@@ -1,14 +1,17 @@
+"use strict";
+
 // Move every letter in the provided string forward 10 letters through the alphabet.
-
 // If it goes past 'z', start again at 'a'.
-
 // Input will be a string with length > 0.
-
 function moveTen(s) {
   moveten = [];
-  let arr = s.split("").map((word) => word.charCodeAt());
+  var arr = s.split("").map(function (word) {
+    return word.charCodeAt();
+  });
+
   for (i = 0; i < arr.length; i++) {
     move = arr[i] + 10;
+
     if (move > 122) {
       move = move - 26;
       moveten.push(move);
@@ -16,7 +19,10 @@ function moveTen(s) {
       moveten.push(move);
     }
   }
-  text = moveten.map((word) => String.fromCharCode(word)).join("");
+
+  text = moveten.map(function (word) {
+    return String.fromCharCode(word);
+  }).join("");
   return text;
 }
 
