0004984497cae025df45f4f10ed55a648300f58c
alexandex <kienva1992@gmail.com>
2021-08-03 18:21:23 +0300

    1

--- JavaScript/move10.js
+++ JavaScript/move10.js
@@ -5,17 +5,17 @@
 // Input will be a string with length > 0.
 
 function moveTen(s) {
-    moveten = [];
-    let arr = s.split("").map((word) => word.charCodeAt());
-    for (i = 0; i < arr.length; i++) {
-      move = arr[i] + 10;
-      if (move > 122) {
-        move = move - 26;
-        moveten.push(move);
-      } else {
-        moveten.push(move);
-      }
+  moveten = [];
+  let arr = s.split("").map((word) => word.charCodeAt());
+  for (i = 0; i < arr.length; i++) {
+    move = arr[i] + 10;
+    if (move > 122) {
+      move = move - 26;
+      moveten.push(move);
+    } else {
+      moveten.push(move);
     }
-    text = moveten.map((word) => String.fromCharCode(word)).join("");
-    return text;
+  }
+  text = moveten.map((word) => String.fromCharCode(word)).join("");
+  return text;
 }
