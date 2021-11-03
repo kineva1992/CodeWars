bc29ac1b011ab49df548b5a1d62faa3bb97a3bf9
alexandex <kienva1992@gmail.com>
2021-11-03 19:36:49 +0300

    123

--- C#/kyu7/Filter the number/Program.cs
+++ C#/kyu7/Filter the number/Program.cs
@@ -4,9 +4,19 @@ namespace Filter_the_number
 {
     class Program
     {
+        public static int FilterString(string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsLetter(s[i]))
+                    result += s[i];
+            }
+            return Convert.ToInt32(result);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(FilterString("aa1bb2cc3dd"));
         }
     }
 }
