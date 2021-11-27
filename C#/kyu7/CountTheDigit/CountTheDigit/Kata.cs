480aca9373373e897ffb86c01d6e6e49ac940e06
Alexandr Kuizn <kineva1992@gmail.com>
2021-11-27 15:07:25 +0300

    123

---
+++ C#/kyu7/CountTheDigit/CountTheDigit/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheDigit
+{
+    internal class Kata
+    {
+        public static int NbDig(int n, int d)
+        {
+            int result = 0;
+            
+            for (int i = 0; i <= n; i++)
+            {
+                int square = i * i;
+                result += square.ToString().Count(x => x == d.ToString()[0]);
+            }
+
+            return result;
+        }
+    }
+}
